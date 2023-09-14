37cffaedd34d1745118f5ae438a4ba1697c962ff
Ben Cooper <39800139+BenACooper@users.noreply.github.com>
2023-09-14 00:17:04 -0400

    added db.js and query functions, index.js now simply calls these functions

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
-//import query functions
+//import prepared statements
 const {
-    viewAllDepartments,
-    viewAllRoles,
-    viewAllEmployees,
-    addDepartment,
-    addRole,
-    addEmployee,
-    updateEmployeeRole,
-  } = require('./queries.js')
+  viewAllDepartments,
+  viewAllRoles,
+  viewAllEmployees,
+  addDepartment,
+  addRole,
+  addEmployee,
+  updateEmployeeRole,
+} = require("./db");
 
 // Create a MySQL connection
 const connection = mysql.createConnection({
@@ -23,23 +23,56 @@ const connection = mysql.createConnection({
 // WHEN I start the application
 // THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
 function startApp() {
-  inquirer.prompt([
-    {
-      type: "list",
-      name: "menuChoice",
-      message: "What woulkd you like to do?",
-      choices: [
-        "View all departments",
-        "View all roles",
-        "View all employees",
-        "Add a department",
-        "Add a role",
-        "Add an employee",
-        "Update an employee role",
-        "Exit", // You can add an exit option if needed
-      ],
-    },
-  ]).then((voice))
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "menuChoice",
+        message: "What woulkd you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+          "Exit", // You can add an exit option if needed
+        ],
+      },
+    ])
+    .then((answers) => {
+      // Handle the user's choice here
+      const choice = answers.menuChoice;
+
+      // Call the correct prepared statements based on the user's choice
+      switch (choice) {
+        case "View all departments":
+          viewAllDepartments();
+          break;
+        case "View all roles":
+          viewAllRoles();
+          break;
+        case "View all employees":
+          viewAllEmployees();
+          break;
+        case "Add a department":
+          addDepartemnt();
+          break;
+        case "Add a role":
+          addRole();
+          break;
+        case "Add an employee":
+          addEmployee();
+          break;
+        case "Update an employee role":
+          updateEmployeeRole();
+          break;
+        case "Exit":
+          console.log("Exiting the application.");
+          process.exit(); // Terminate the Node.js process
+      }
+    });
 }
 
 // Call the startApp function to begin the application
